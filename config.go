6cbae6501b73e64699390dc03c60fe624d1f9f72
Maciej Dobrzanski <git@dbnski.com>
2026-02-16 13:49:18 +0100

    Split loadConfigFile()

---
+++ config.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+
+    "gopkg.in/ini.v1"
+    "github.com/go-sql-driver/mysql"
+)
+
+func loadConfigFile(c *mysql.Config, defaultsFile string, defaultsGroup string) error {
+    options := ini.LoadOptions{
+        AllowBooleanKeys: true,
+        IgnoreContinuation: true,
+        IgnoreInlineComment: true,
+    }
+
+    params, err := ini.LoadSources(options, defaultsFile)
+    if err != nil {
+        return err
+    }
+
+    for _, s := range params.Sections() {
+        if s.Name() != defaultsGroup {
+            continue
+        }
+
+        if c.User == "" && s.Key("user").String() != "" {
+            c.User = s.Key("user").String()
+        }
+        if c.Passwd == "" && s.Key("password").String() != "" {
+            c.Passwd = s.Key("password").String()
+        }
+        if c.Addr == "" && s.Key("socket").String() != "" {
+            c.Net = "unix"
+            c.Addr = s.Key("socket").String()
+        }
+        if c.Addr == "" && s.Key("host").String() != "" {
+            c.Net = "tcp"
+            c.Addr = s.Key("host").String()
+        }
+        if c.Addr == "" && s.Key("port").String() != "" {
+            addr := strings.Split(c.Addr, ":")
+            c.Addr = addr[0] + ":" + s.Key("port").String()
+        }
+
+        return nil
+    }
+
+    return fmt.Errorf("Client section not found")
+}
--- main.go
+++ main.go
@@ -3,17 +3,14 @@ package main
 import (
     "context"
     "database/sql"
-    "fmt"
     "log/slog"
     "os"
     "os/signal"
-    "strings"
     "sync"
     "syscall"
     "time"
 
     "github.com/alecthomas/kong"
-    "gopkg.in/ini.v1"
     "github.com/go-sql-driver/mysql"
     "golang.org/x/term"
 )
@@ -57,48 +54,6 @@ var options struct {
                                  placeholder:"PATH" 
                                  default:"." 
                                  help:"Output directory"`
-}
-
-func loadConfigFile(c *mysql.Config, defaultsFile string, defaultsGroup string) error {
-    options := ini.LoadOptions{
-        AllowBooleanKeys: true,
-        IgnoreContinuation: true,
-        IgnoreInlineComment: true,
-    }
-
-    params, err := ini.LoadSources(options, defaultsFile)
-    if err != nil {
-        return err
-    }
-
-    for _, s := range params.Sections() {
-        if s.Name() != defaultsGroup {
-            continue
-        }
-
-        if c.User == "" && s.Key("user").String() != "" {
-            c.User = s.Key("user").String()
-        }
-        if c.Passwd == "" && s.Key("password").String() != "" {
-            c.Passwd = s.Key("password").String()
-        }
-        if c.Addr == "" && s.Key("socket").String() != "" {
-            c.Net = "unix"
-            c.Addr = s.Key("socket").String()
-        }
-        if c.Addr == "" && s.Key("host").String() != "" {
-            c.Net = "tcp"
-            c.Addr = s.Key("host").String()
-        }
-        if c.Addr == "" && s.Key("port").String() != "" {
-            addr := strings.Split(c.Addr, ":")
-            c.Addr = addr[0] + ":" + s.Key("port").String()
-        }
-
-        return nil
-    }
-
-    return fmt.Errorf("Client section not found")
 }
 
 func main() {
